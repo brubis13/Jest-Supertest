bfed56402d2068024db03a87e51d170cd8ad88fa
Bruna Neves <nevesbm13@gmail.com>
2023-08-08 13:13:42 +0300

    add news tests

--- __tests__/swapi.spec.js
+++ __tests__/swapi.spec.js
@@ -12,19 +12,66 @@ describe('Testes da API de Pessoas', () => {
     return resposta;
   };
 
+  it('Deve retornar status 200 e informações corretas para a pessoa com ID 1',() => {
+
+    // Verifica se o status é 200
+    expect(resposta.status).toBe(200);
+
+    // Verifica se as propriedades esperadas estão presentes e com os valores corretos
+    expect(resposta.body).toHaveProperty('name', 'Luke Skywalker');
+    expect(resposta.body).toHaveProperty('height', '172');
+    expect(resposta.body).toHaveProperty('mass', '77');
+    expect(resposta.body).toHaveProperty('hair_color', 'blond');
+    expect(resposta.body).toHaveProperty('skin_color', 'fair');
+    expect(resposta.body).toHaveProperty('eye_color', 'blue');
+    expect(resposta.body).toHaveProperty('birth_year', '19BBY');
+    expect(resposta.body).toHaveProperty('gender', 'male');
+    expect(resposta.body).toHaveProperty('homeworld', 'https://swapi.dev/api/planets/1/');
+
+    // Verifica se a propriedade "films" contém os filmes corretos
+    expect(resposta.body).toHaveProperty('films', [
+      'https://swapi.dev/api/films/1/',
+      'https://swapi.dev/api/films/2/',
+      'https://swapi.dev/api/films/3/',
+      'https://swapi.dev/api/films/6/'
+    ]);
+
+    // Verifica se a propriedade "species" contém o array vazio, indicando que não há informações de espécie
+    expect(resposta.body.species).toEqual([]);
+
+    // Verifica se a propriedade "vehicles" contém os veículos corretos
+    expect(resposta.body).toHaveProperty('vehicles', [
+      'https://swapi.dev/api/vehicles/14/',
+      'https://swapi.dev/api/vehicles/30/'
+    ]);
+
+    // Verifica se a propriedade "starships" contém as espaçonaves corretas
+    expect(resposta.body).toHaveProperty('starships', [
+      'https://swapi.dev/api/starships/12/',
+      'https://swapi.dev/api/starships/22/'
+    ]);
+
+    // Verifica se as propriedades "created" e "edited" existem
+    expect(resposta.body).toHaveProperty('created');
+    expect(resposta.body).toHaveProperty('edited');
+
+    // Verifica se a propriedade "url" contém um URL válida
+    expect(resposta.body.url).toMatch(/^https:\/\/swapi\.dev\/api\/people\/\d+\/$/);
+  });
+
   it.skip('', async () => {
     console.log(resposta.body);
     console.log(resposta.status);
   });
 
-  it('Deve visualizar informações de cadastro quando buscar por uma pessoa existente', async () => {
+  it.skip('Deve visualizar informações de cadastro quando buscar por uma pessoa existente', () => {
     expect(resposta.status).toBe(200);
     expect(resposta.body.films).toBeDefined();
     expect(resposta.body.vehicles.length).toBeGreaterThan(0);
     expect(resposta.body.name).toBe('Luke Skywalker');
   });
 
-  it('Deve retornar um erro que a pessoa não existe', async () => {
+  it.skip('Deve retornar um erro que a pessoa não existe', async () => {
     const resposta = await getPersonById(9999);
 
     console.log(resposta.status);
